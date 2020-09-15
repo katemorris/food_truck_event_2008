9252599b817040f10ab01d62d7cc9d4aa78d4120
Kate Tester <katemorris@gmail.com>
2020-09-15 13:35:14 -0600

    Adds event test for attributes to start.

---
+++ test/event_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+class EventTest < Minitest::Test
+  def test_it_has_attributes
+    event = Event.new("South Pearl Street Farmers Market")
+
+    assert_equal "South Pearl Street Farmers Market", event.name
+    assert_equal [], event.food_trucks
+  end
+end
