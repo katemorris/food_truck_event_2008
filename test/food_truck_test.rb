2c7fdcba1353ab7cd04f360ce00698ed07154e40
Kate Tester <katemorris@gmail.com>
2020-09-15 13:16:09 -0600

    Setups up attributes test.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -4,9 +4,12 @@ require './lib/item'
 require './lib/food_truck'
 
 class FoodTruckTest < Minitest::Test
-  def setup
+  def test_it_has_attributes
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_equal "Rocky Mountain Pies", food_truck.name
+    assert_empty food_truck.inventory
   end
 end
