6f36cad8cf26bfde0c15ea836bdb9502fb9fe20d
Kate Tester <katemorris@gmail.com>
2020-09-15 13:08:26 -0600

    Adds setup for foodtruck class tests

---
+++ test/food_truck_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+  def setup
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+end
