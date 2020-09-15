c7a62c79818c1c73ddfccc657d5baaa2aa8f494f
Kate Tester <katemorris@gmail.com>
2020-09-15 13:20:34 -0600

    Adds test to check if truck can be stocked.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -12,4 +12,24 @@ class FoodTruckTest < Minitest::Test
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_empty food_truck.inventory
   end
+
+  def test_it_can_stock_the_truck
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+
+    assert_equal {item1 => 30}, food_truck.inventory
+    assert_equal 30, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 25)
+    assert_equal 55, food_truck.check_stock(item1)
+
+    food_truck.stock(item2, 12)
+
+    assert_equal {item1 => 55, item2 => 12}, food_truck.inventory
+  end
 end
